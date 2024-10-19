a855518401c51d688916c55d736f5f86d21e1321
Mikey <67529953+Mikey-Mikey@users.noreply.github.com>
2024-10-19 19:54:39 -0400

    adding an audio visualizer

--- gamemode/cl_hud.lua
+++ gamemode/cl_hud.lua
@@ -1,3 +1,16 @@
+radio_station = radio_station or nil -- garbage collection prevention
+
+print("Creating radio station")
+sound.PlayURL("https://radio.blueberry.coffee/radio.mp3", "noplay", function(station, errorID, err) -- play my friend's radio station for some background music while playing
+    if IsValid(station) then
+        station:Play()
+        station:SetVolume(0.25)
+        radio_station = station
+    end
+
+end)
+
+
 hook.Add("HUDPaint", "DrawRoundTime", function()
     local timeLeft = GetGlobal2Float("CurrentRoundTime", 0)
 
@@ -15,6 +28,23 @@ hook.Add("HUDPaint", "DrawRoundTime", function()
     draw.RoundedBox(8, ScrW() / 2 - 100, 25, 200, 50, Color(0,0,0,200))
 
     draw.SimpleText(timerText, "DermaLarge", ScrW() / 2, 50, Color(255,151,48), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+
+    -- draw fft spectrum of the radio station at the bottom of the screen
+
+    if IsValid(radio_station) then
+        local spectrum = radio_station:GetFFT()
+        local spectrumWidth = ScrW() / 2
+        local spectrumHeight = 100
+        local spectrumX = ScrW() / 2 - spectrumWidth / 2
+        local spectrumY = ScrH() - spectrumHeight - 25
+
+        draw.RoundedBox(8, spectrumX, spectrumY, spectrumWidth, spectrumHeight, Color(0,0,0,200))
+
+        for i = 1, #spectrum do
+            local height = spectrum[i] * spectrumHeight
+            draw.RoundedBox(0, spectrumX + i, spectrumY + spectrumHeight - height, 1, height, Color(255,151,48))
+        end
+    end
 end)
 
 
