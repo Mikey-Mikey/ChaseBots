a3d96e7d7bb7f5e58f7612fa11f08336095e6169
Mikey <67529953+Mikey-Mikey@users.noreply.github.com>
2024-10-19 19:42:57 -0400

    fixed garbage collection issue

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -2,6 +2,7 @@ include("shared.lua")
 include("cl_hooks.lua")
 include("cl_hud.lua")
 
+local radio_station = nil -- garbage collection prevention
 timer.Simple(1, function()
     print("Creating radio station")
     sound.PlayURL("https://radio.blueberry.coffee/radio.mp3", "noplay", function(station, errorID, err) -- play my friend's radio station for some background music while playing
@@ -9,6 +10,7 @@ timer.Simple(1, function()
         if IsValid(station) then
             station:Play()
             station:SetVolume(0.5)
+            radio_station = station
         end
 
     end)
