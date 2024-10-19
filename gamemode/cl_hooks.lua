5a89f542fbbf69887076ef53ee68578675f58bbc
Mikey <67529953+Mikey-Mikey@users.noreply.github.com>
2024-10-19 05:42:35 -0400

    removed

--- gamemode/cl_hooks.lua
+++ gamemode/cl_hooks.lua
@@ -6,14 +6,3 @@ hook.Add("NetworkEntityCreated", "SetRagdollColors", function(ent)
         end
     end
 end)
-
-hook.Add("EntityEmitSound", "FixQuietSounds", function(data)
-    if not string.StartsWith(data.Entity:GetClass(), "npc_") then return end
-    timer.Remove(data.Entity:EntIndex() .. "FixQuietSounds")
-    timer.Create(data.Entity:EntIndex() .. "FixQuietSounds", 0, 0, function()
-        if not IsValid(data.Entity) then return end
-        data.Volume = 1
-    end)
-
-    return true
-end)
