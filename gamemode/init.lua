dac16881d1801aa57199516e05d3f7fbf0be984a
Mikey <67529953+Mikey-Mikey@users.noreply.github.com>
2024-10-19 03:39:43 -0400

    raised nextbot spawn distance

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -26,7 +26,7 @@ local function GetRandomPointOnNavMesh()
         for k, ply in ipairs(player.GetAll()) do
             local distSqr = ply:GetPos():DistToSqr(area:GetCenter())
 
-            if distSqr < 1000^2 then
+            if distSqr < 2000^2 then
                 return false
             end
         end
