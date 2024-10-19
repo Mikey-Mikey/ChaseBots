ff1b571f48e3d3b510e6e36ead6e26337f6c6862
Mikey <67529953+Mikey-Mikey@users.noreply.github.com>
2024-10-19 19:18:13 -0400

    gaben will show something to you

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -2,17 +2,13 @@ include("shared.lua")
 include("cl_hooks.lua")
 include("cl_hud.lua")
 
-gameevent.Listen("player_activate")
-hook.Add("player_activate", "player_activate_example", function(data)
-    local userid = data.userid
-    timer.Simple(1, function()
-        if userid == LocalPlayer():UserID() then
-            sound.PlayURL("https://radio.blueberry.coffee/radio.ogg", "noplay", function(station) -- play my friend's radio station for some background music while playing
-                if IsValid(station) then
-                    station:Play()
-                    station:SetVolume(0.5)
-                end
-            end)
+timer.Simple(1, function()
+    print("Creating radio station")
+    sound.PlayURL("https://radio.blueberry.coffee/radio.ogg", "noplay", function(station) -- play my friend's radio station for some background music while playing
+        if IsValid(station) then
+            print("Playing radio station")
+            station:Play()
+            station:SetVolume(0.5)
         end
     end)
 end)
