3a8cbb2ef4253ffca57003229a99e41f6e6e886a
Mikey <67529953+Mikey-Mikey@users.noreply.github.com>
2024-10-19 19:27:06 -0400

    asd

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -5,6 +5,7 @@ include("cl_hud.lua")
 timer.Simple(1, function()
     print("Creating radio station")
     sound.PlayURL("https://radio.blueberry.coffee/radio.ogg", "noplay", function(station, errorID, err) -- play my friend's radio station for some background music while playing
+        print("Created radio station")
         if IsValid(station) then
             print("Playing radio station")
             station:Play()
