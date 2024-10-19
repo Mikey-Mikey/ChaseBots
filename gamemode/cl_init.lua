fc4b3d652a7e23a5e5bdb6b5625f00253cc929bc
Mikey <67529953+Mikey-Mikey@users.noreply.github.com>
2024-10-19 19:15:09 -0400

    fix for radio station?

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -2,9 +2,15 @@ include("shared.lua")
 include("cl_hooks.lua")
 include("cl_hud.lua")
 
-sound.PlayURL("https://radio.blueberry.coffee/radio.ogg", "noplay", function(station) -- play my friend's radio station for some background music while playing
-    if IsValid(station) then
-        station:Play()
-        station:SetVolume(0.5)
+gameevent.Listen("player_activate")
+hook.Add("player_activate", "player_activate_example", function(data)
+    local userid = data.userid
+    if userid == LocalPlayer():UserID() then
+        sound.PlayURL("https://radio.blueberry.coffee/radio.ogg", "noplay", function(station) -- play my friend's radio station for some background music while playing
+            if IsValid(station) then
+                station:Play()
+                station:SetVolume(0.5)
+            end
+        end)
     end
 end)
