294092d59ad6398f33818412926a45e92cfed701
Mikey <67529953+Mikey-Mikey@users.noreply.github.com>
2024-10-19 19:16:29 -0400

    asd

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -5,12 +5,14 @@ include("cl_hud.lua")
 gameevent.Listen("player_activate")
 hook.Add("player_activate", "player_activate_example", function(data)
     local userid = data.userid
-    if userid == LocalPlayer():UserID() then
-        sound.PlayURL("https://radio.blueberry.coffee/radio.ogg", "noplay", function(station) -- play my friend's radio station for some background music while playing
-            if IsValid(station) then
-                station:Play()
-                station:SetVolume(0.5)
-            end
-        end)
-    end
+    timer.Simple(1, function()
+        if userid == LocalPlayer():UserID() then
+            sound.PlayURL("https://radio.blueberry.coffee/radio.ogg", "noplay", function(station) -- play my friend's radio station for some background music while playing
+                if IsValid(station) then
+                    station:Play()
+                    station:SetVolume(0.5)
+                end
+            end)
+        end
+    end)
 end)
