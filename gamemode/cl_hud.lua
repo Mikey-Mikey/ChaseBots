dfe39965f00d50fedbe50a6742f1d596ce9dc802
Mikey <67529953+Mikey-Mikey@users.noreply.github.com>
2024-10-20 01:29:59 -0400

    123

--- gamemode/cl_hud.lua
+++ gamemode/cl_hud.lua
@@ -60,13 +60,13 @@ hook.Add("HUDPaint", "DrawRoundTime", function()
             end
 
             local xPos = i * spacing - spacing * 2
-            if i == 2 then continue end
             local brightness = math.min(math.sqrt(height / spectrumPower) * 2 + 0.25, 1)
             -- do proper visualization of the spectrum
             draw.RoundedBox(barWidth, xPos + math.floor(ScrW() * 0.5), spectrumY - barHeights[i] + barWidth + 3, barWidth, barHeights[i] * 2, Color(0, 0, 0, 127))
 
             draw.RoundedBox(barWidth, xPos + math.floor(ScrW() * 0.5), spectrumY - barHeights[i] + barWidth, barWidth, barHeights[i] * 2, HSVToColor(0, 0, brightness))
             
+            if i == 2 then continue end
 
             draw.RoundedBox(barWidth, -xPos + math.floor(ScrW() * 0.5), spectrumY - barHeights[i] + barWidth + 3, barWidth, barHeights[i] * 2, Color(0, 0, 0, 127))
 
