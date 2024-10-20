0e277b79362c67ef53d47a2ea4f7e6feab59fe19
Mikey <67529953+Mikey-Mikey@users.noreply.github.com>
2024-10-19 23:53:18 -0400

    a

--- gamemode/cl_hud.lua
+++ gamemode/cl_hud.lua
@@ -54,8 +54,11 @@ hook.Add("HUDPaint", "DrawRoundTime", function()
                 barHeights[i] = Lerp(0.3, barHeights[i] or 0, height)
             end
 
+            local xPos = (i - 1) / table.Count(spectrum) * (spectrumWidth - barWidth)
+            xPos = xPos - (spectrumWidth - barWidth) / table.Count(spectrum)
+
             -- do proper visualization of the spectrum
-            draw.RoundedBox(barWidth * 2, (i - 1) / table.Count(spectrum) * (spectrumWidth - barWidth), spectrumY + spectrumHeight - barHeights[i], barWidth, barHeights[i], HSVToColor(i / #spectrum * 360, 1, 1))
+            draw.RoundedBox(barWidth * 2,xPos , spectrumY + spectrumHeight - barHeights[i], barWidth, barHeights[i], HSVToColor(i / #spectrum * 360, 1, 1))
         end
     end
 end)
