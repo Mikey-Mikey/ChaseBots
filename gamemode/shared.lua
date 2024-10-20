e98d1cf1c3b035dd79d245d08aead6ab217275cf
Mikey <67529953+Mikey-Mikey@users.noreply.github.com>
2024-10-20 05:39:38 -0400

    nevermind later plans

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -123,7 +123,3 @@ hook.Add("Move", "SpectatorMovement", function( ply, mv )
     return true
 
 end)
-
-hook.Add("PlayerSwitchWeapon", "DisableWeaponSwitching", function( ply, oldWeapon, newWeapon )
-    return true
-end)
