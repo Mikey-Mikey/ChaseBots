619169b2f9afa97491cc54bc7235c207c7faf10e
Mikey <67529953+Mikey-Mikey@users.noreply.github.com>
2024-10-20 00:19:00 -0400

    Used LerpExpo to make the bars frame independent

--- gamemode/cl_hud.lua
+++ gamemode/cl_hud.lua
@@ -54,7 +54,7 @@ hook.Add("HUDPaint", "DrawRoundTime", function()
             if height < barHeights[i] then
                 barHeights[i] = math.max(0, barHeights[i] - 2)
             else
-                barHeights[i] = Lerp(0.15, barHeights[i] or 0, height)
+                barHeights[i] = LerpExpo(FrameTime(), barHeights[i] or 0, height, 5)
             end
 
             local xPos = (i - 1) / table.Count(spectrum) * (spectrumWidth - barWidth)
