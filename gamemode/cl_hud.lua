1b787ed9246274c55a38a41acad0b6d79747fbab
Mikey <67529953+Mikey-Mikey@users.noreply.github.com>
2024-10-20 01:12:53 -0400

    drop shadow

--- gamemode/cl_hud.lua
+++ gamemode/cl_hud.lua
@@ -63,6 +63,8 @@ hook.Add("HUDPaint", "DrawRoundTime", function()
             xPos = xPos + math.floor(ScrW() * 0.5 - #spectrum * spacing * 0.5)
 
             -- do proper visualization of the spectrum
+            draw.RoundedBox(barWidth, xPos + 3, spectrumY - barHeights[i] + barWidth + 3, barWidth, barHeights[i] * 2, Color(0, 0, 0))
+
             draw.RoundedBox(barWidth, xPos, spectrumY - barHeights[i] + barWidth, barWidth, barHeights[i] * 2, HSVToColor(0, 0, math.sqrt(height / spectrumPower) + 0.25))
         end
     end
