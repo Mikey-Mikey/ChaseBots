a45c01cc30b0db8d344915774f82c9d816df1768
Mikey <67529953+Mikey-Mikey@users.noreply.github.com>
2024-10-21 17:32:32 -0400

    asd

--- gamemode/cl_hud.lua
+++ gamemode/cl_hud.lua
@@ -231,7 +231,7 @@ local function DrawPlayerRow(ply, x, y, w, h)
     if not IsValid(ply) then return end
     local roleColor = ROLE_COLORS[ply:GetUserGroup()] or color_white
 
-    draw.RoundedBox(10, x - 2, y - 2, w + 4, h + 4, Color(roleColor.r, roleColor.g, roleColor.b, 200))
+    draw.RoundedBox(12, x - 2, y - 2, w + 4, h + 4, Color(roleColor.r, roleColor.g, roleColor.b, 200))
 
     draw.RoundedBox(8, x, y, w, h, Color(0, 0, 0, 255))
 
