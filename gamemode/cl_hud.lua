da96eee40948cd479d02dea5491add2d1c07f1c3
Mikey <67529953+Mikey-Mikey@users.noreply.github.com>
2024-10-21 18:42:19 -0400

    asd

--- gamemode/cl_hud.lua
+++ gamemode/cl_hud.lua
@@ -309,9 +309,9 @@ hook.Add("HUDDrawScoreBoard", "Scoreboard", function()
         return rolePriorityA < rolePriorityB
     end)
 
-    draw.RoundedBoxEx(12, x + 8, rowY, w - 16, h - 9 - 90, Color(0, 0, 0, 127), true, true, false, false)
+    draw.RoundedBoxEx(12, x + 8, rowY, w - 16, h - 8 - 90, Color(0, 0, 0, 127), true, true, false, false)
 
-    render.SetScissorRect(x + 8, rowY - 2, x + w - 8, y + h - 9 - 90, true)
+    render.SetScissorRect(x + 8, rowY - 2, x + w - 8, y + h - 8 - 90, true)
 
     for i, ply in pairs(players) do
         DrawPlayerRow(ply, x + 10, rowY, w - 20, rowHeight)
