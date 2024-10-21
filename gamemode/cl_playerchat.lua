36fe1dbf732b0fd26b57d42a293b4eafde42a813
Mikey <67529953+Mikey-Mikey@users.noreply.github.com>
2024-10-21 18:46:04 -0400

    Auto send the discord invite every 4 minutes

--- gamemode/cl_playerchat.lua
+++ gamemode/cl_playerchat.lua
@@ -7,3 +7,7 @@ net.Receive("PlayerChatted", function()
 
     chat.AddText(Color(255,246,43), "[", roleColor, role, Color(255,246,43), "] ", roleColor, ply:Nick(), color_white, ": " .. text)
 end)
+
+timer.Create("ShowDiscordInvite", 240, 0, function()
+    chat.AddText(Color(43,142,255), "Join our Discord server!: ", Color(187, 128, 255), "https://discord.gg/SHaYKb4AFw")
+end)
