539801dfb0d7e1063e6fd00793a1069252e9a47f
Mikey <67529953+Mikey-Mikey@users.noreply.github.com>
2024-10-21 17:26:27 -0400

    asd

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -1,3 +1,11 @@
+ROLE_COLORS = {
+    ["Owner"] = Color(255, 0, 98),
+    ["Co-Owner"] = Color(180, 0, 69),
+    ["Admin"] = Color(255, 0, 0),
+    ["Regular"] = Color(0,119,255),
+    ["Guest"] = Color(255, 211, 144),
+}
+
 include("shared.lua")
 include("cl_hooks.lua")
 include("cl_hud.lua")
--- gamemode/cl_playerchat.lua
+++ gamemode/cl_playerchat.lua
@@ -1,17 +1,9 @@
-local roleColors = {
-    ["Owner"] = Color(255, 0, 98),
-    ["Co-Owner"] = Color(180, 0, 69),
-    ["Admin"] = Color(255, 0, 0),
-    ["Regular"] = Color(0,119,255),
-    ["Guest"] = Color(255, 211, 144),
-}
-
 net.Receive("PlayerChatted", function()
     local ply = net.ReadEntity()
     local text = net.ReadString()
-    roleColor = roleColors[ply:GetUserGroup()] or color_white
+    roleColor = ROLE_COLORS[ply:GetUserGroup()] or color_white
 
-    local role = table.KeyFromValue(roleColors, roleColor)
+    local role = table.KeyFromValue(ROLE_COLORS, roleColor)
 
     chat.AddText(Color(255,246,43), "[", roleColor, role, Color(255,246,43), "] ", roleColor, ply:Nick(), color_white, ": " .. text)
 end)
