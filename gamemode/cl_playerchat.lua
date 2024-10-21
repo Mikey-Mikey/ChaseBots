1d82360309a651efb1070f15d0df42438be6863c
Mikey <67529953+Mikey-Mikey@users.noreply.github.com>
2024-10-21 16:02:09 -0400

    asd

--- gamemode/cl_playerchat.lua
+++ gamemode/cl_playerchat.lua
@@ -1,12 +1,13 @@
 local roleColors = {
-    ["superadmin"] = Color(0, 140, 255),
-    ["admin"] = Color(0, 255, 0),
-    ["user"] = color_white
+    ["Owner"] = Color(0, 140, 255),
 }
 
 net.Receive("PlayerChatted", function()
     local ply = net.ReadEntity()
     local text = net.ReadString()
     roleColor = roleColors[ply:GetUserGroup()] or color_white
-    chat.AddText(Color(255,246,43), "[", roleColor, ply:Nick(), Color(255,246,43), "]", color_white, ": " .. text)
+
+    local role = table.GetKeys(roleColors)[table.KeyFromValue(roleColors, roleColor)]
+
+    chat.AddText(Color(255,246,43), "[", roleColor, role, Color(255,246,43), "] ", roleColor, ply:Nick(), color_white, ": " .. text)
 end)
