cb04dbb00caa3cc35ec8f2a4457ec59b8598b00c
Mikey <67529953+Mikey-Mikey@users.noreply.github.com>
2024-10-22 17:38:14 -0400

    allow flashlights

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -102,6 +102,7 @@ function GM:StartRound()
         ply:SetNWBool("Spectating", false)
         ply:GodDisable()
         ply:Spawn()
+        ply:AllowFlashlight(true)
     end
 
     GAMEMODE.CurrentNextbots = {}
