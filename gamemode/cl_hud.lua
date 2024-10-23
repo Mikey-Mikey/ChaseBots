53aa6e52beda85d04177f3fd403d79138c1362a7
Mikey <67529953+Mikey-Mikey@users.noreply.github.com>
2024-10-23 18:06:39 -0400

    asd

--- gamemode/cl_hud.lua
+++ gamemode/cl_hud.lua
@@ -249,9 +249,23 @@ hook.Add("HUDPaint", "DrawRoundTime", function()
 
     draw.RoundedBox(minimapSize, minimapX - minimapSize * 0.5, minimapY - minimapSize * 0.5, minimapSize, minimapSize, Color(0, 0, 0, 255))
 
-    surface.SetMaterial(minimapMat)
-    surface.DrawTexturedRect(minimapX - minimapSize * 0.5, minimapY - minimapSize * 0.5, minimapSize, minimapSize)
-    draw.NoTexture()
+    minimapDraw = true
+    local oldClip = render.EnableClipping(true)
+    render.RenderView({
+        origin = LocalPlayer():GetPos() + Vector(0,0,80),
+        angles = Angle(-90, 0, 0),
+        drawviewmodel = false,
+        drawviewer = true,
+        x = minimapX - minimapSize * 0.5, y = minimapY - minimapSize * 0.5,
+        w = minimapSize, h = minimapSize,
+        ortho = {}
+    })
+    render.EnableClipping(oldClip)
+    minimapDraw = false
+
+    --surface.SetMaterial(minimapMat)
+    --surface.DrawTexturedRect(minimapX - minimapSize * 0.5, minimapY - minimapSize * 0.5, minimapSize, minimapSize)
+    --draw.NoTexture()
 
     if LocalPlayer():GetNWBool("KillingSoon", false) then
         draw.SimpleText("Warning: You will be killed soon if you are inactive.", "DermaLarge", ScrW() / 2, ScrH() / 2, Color(255, 0, 0), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
@@ -266,19 +280,6 @@ end)
 
 hook.Add("RenderScene", "MinimapRender", function(origin, angles, fov)
     render.PushRenderTarget(minimapRT)
-    minimapDraw = true
-    local oldClip = render.EnableClipping(true)
-    render.RenderView({
-        origin = LocalPlayer():GetPos() + Vector(0,0,80),
-        angles = Angle(-90, 0, 0),
-        drawviewmodel = false,
-        drawviewer = true,
-        x = minimapX - minimapSize * 0.5, y = minimapY - minimapSize * 0.5,
-        w = minimapSize, h = minimapSize,
-        ortho = {}
-    })
-    render.EnableClipping(oldClip)
-    minimapDraw = false
     render.PopRenderTarget()
 end)
 
