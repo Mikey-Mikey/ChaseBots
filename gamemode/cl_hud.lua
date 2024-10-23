be632973bc3d28f98e595e717bd34d0ef72d4d47
Mikey <67529953+Mikey-Mikey@users.noreply.github.com>
2024-10-23 17:35:16 -0400

    asd

--- gamemode/cl_hud.lua
+++ gamemode/cl_hud.lua
@@ -247,7 +247,7 @@ hook.Add("HUDPaint", "DrawRoundTime", function()
     local oldClip = render.EnableClipping(true)
     render.RenderView({
         origin = LocalPlayer():GetPos() + Vector(0,0,80),
-        angles = Angle(-90, 0, 0),
+        angles = Angle(90, 0, 0),
         ortho = {
             top = 80,
             bottom = -500,
