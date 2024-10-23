e707aefe1ee402342358f0c4b54ba3ff88fc89b4
Mikey <67529953+Mikey-Mikey@users.noreply.github.com>
2024-10-23 01:46:36 -0400

    stardraw

--- entities/entities/nextbot_spawnpoint.lua
+++ entities/entities/nextbot_spawnpoint.lua
@@ -56,15 +56,48 @@ function ENT:Initialize()
             end
         mesh.End()
 
+        --[[ p5js code
+            let prevAng = radians(a - 360 / 5 + 180);
+            let ang = radians(a + 360 / 5 + 180);
+            
+            line(sin(prevAng) * 20,cos(prevAng) * 20,sin(ang) * 20,cos(ang) * 20);
+        ]]
+        -- draw a line using 4 vertices
+        local function drawLine(x1, y1, x2, y2)
+            local dir = Vector(x2 - x1, y2 - y1, 0):GetNormalized()
+            local perp = Vector(-dir.y, dir.x, 0) * 5
+
+            mesh.Color(127, 0, 0, 255)
+            mesh.Position(Vector(x1 + perp.x, y1 + perp.y, 0))
+            mesh.AdvanceVertex()
+
+            mesh.Color(127, 0, 0, 255)
+            mesh.Position(Vector(x1 - perp.x, y1 - perp.y, 0))
+            mesh.AdvanceVertex()
+
+            mesh.Color(127, 0, 0, 255)
+            mesh.Position(Vector(x2 + perp.x, y2 + perp.y, 0))
+            mesh.AdvanceVertex()
+
+            mesh.Color(127, 0, 0, 255)
+            mesh.Position(Vector(x2 - perp.x, y2 - perp.y, 0))
+            mesh.AdvanceVertex()
+
+        end
+
         self.starMesh = Mesh()
-        mesh.Begin(self.starMesh, MATERIAL_LINE_STRIP, 360 / 5 * 2)
-            for a = 0, 360, 5 do
-                local ang = math.rad(a)
+        mesh.Begin(self.starMesh, MATERIAL_TRIANGLE_STRIP, 360 / 5 * 2)
+            for a = 0, 360, 360 / 5 do
+                local prevAng = math.rad(a - 360 / 5 + 180)
+                local ang = math.rad(a + 360 / 5 + 180)
+
+                local px = math.cos(prevAng) * 20
+                local py = math.sin(prevAng) * 20
+
                 local x = math.cos(ang) * radius
                 local y = math.sin(ang) * radius
-                mesh.Color(0, 255, 0, 255)
-                mesh.Position(Vector(x * 1.1, y * 1.1, 0))
-                mesh.AdvanceVertex()
+
+                drawLine(px, py, x, y)
             end
         mesh.End()
 
