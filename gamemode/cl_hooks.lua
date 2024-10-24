b5fa2235c7a8f3fdd61bb9e0cb8ad7f7661a9011
Mikey <67529953+Mikey-Mikey@users.noreply.github.com>
2024-10-23 20:40:46 -0400

    asd

--- gamemode/cl_hooks.lua
+++ gamemode/cl_hooks.lua
@@ -53,7 +53,7 @@ hook.Add("RenderScreenspaceEffects", "DrawRoundTime", function()
             ["$pp_colour_addg"] = 0,
             ["$pp_colour_addb"] = 0,
             ["$pp_colour_brightness"] = 0,
-            ["$pp_colour_contrast"] = 1,
+            ["$pp_colour_contrast"] = 1 + grayAmount,
             ["$pp_colour_colour"] = 1 - grayAmount,
             ["$pp_colour_mulr"] = 0,
             ["$pp_colour_mulg"] = 0,
