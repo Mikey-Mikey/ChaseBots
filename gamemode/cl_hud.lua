35ee0ab783780f4452b0fef06d7aad071f4a5f64
Mikey <67529953+Mikey-Mikey@users.noreply.github.com>
2024-10-27 16:01:25 -0400

    asd

--- gamemode/cl_hud.lua
+++ gamemode/cl_hud.lua
@@ -282,7 +282,7 @@ local scoreboardScroll = 0
 
 hook.Add("StartCommand", "ScoreboardScroll", function(ply, cmd)
     if scoreboardShowing and ply == LocalPlayer() then
-        scoreboardScroll = math.Clamp(scoreboardScroll + cmd:GetMouseWheel() * 12, -64 * 48 - 200, 0)
+        scoreboardScroll = math.Clamp(scoreboardScroll + cmd:GetMouseWheel() * 12, -64 * 48, 0)
     end
 end)
 
